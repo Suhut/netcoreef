e785f6ee56e3e35f0cb00154808c6428ae443e2a
Suhut <suhut.wadiyo@gmail.com>
2021-11-13 10:08:58 +0700

    Linq update list dan RuningTotal

--- cobaef/Contoh06Linq.cs
+++ cobaef/Contoh06Linq.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using cobaef.Models;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace cobaef
 {
@@ -19,15 +20,18 @@ namespace cobaef
         public static void cobaQuery()
         {
             var db = new Contoh06Context();
+            //db.Database.Migrate();
 
             //001 Query biasa
             /*
                 SELECT T0.*
                 FROM Invoices T0 
             */
-            var query001 = from T0 in db.Invoices
-                           select T0
-                      ;
+            var query001 = (
+                            from T0 in db.Invoices
+                            select T0
+                           ).AsNoTracking();
+                    ;
             foreach (var invoice in query001)
             {
                 Console.WriteLine($"001 -> {invoice.TransNo}");
@@ -38,9 +42,10 @@ namespace cobaef
                 SELECT TOP 1  T0.*
                 FROM Invoices T0 
             */
-            var query002 = (from T0 in db.Invoices
+            var query002 = (
+                            from T0 in db.Invoices
                             select T0
-                           ).Take(1)
+                           ).AsNoTracking().Take(1)
                       ;
             foreach (var invoice in query002)
             {
@@ -54,9 +59,11 @@ namespace cobaef
                 FROM Invoices T0
                 ORDER BY T0.TransNo ASC
             */
-            var query003 = from T0 in db.Invoices
-                           orderby T0.TransNo ascending  //descending
-                           select T0
+            var query003 = (
+                            from T0 in db.Invoices
+                            orderby T0.TransNo ascending  //descending
+                            select T0
+                           ).AsNoTracking()
                       ;
 
             foreach (var invoice in query003)
@@ -70,9 +77,11 @@ namespace cobaef
                 FROM Invoices T0
                 WHERE T0.TransNo='trans02'
             */
-            var query004 = from T0 in db.Invoices
-                           where T0.TransNo == "trans02"
-                           select T0
+            var query004 = (
+                            from T0 in db.Invoices
+                            where T0.TransNo == "trans02"
+                            select T0
+                           ).AsNoTracking()
                       ;
 
             foreach (var invoice in query004)
@@ -89,11 +98,12 @@ namespace cobaef
                 ORDER BY T0.TransNo ASC
 
             */
-            var query005 = (from T0 in db.Invoices
+            var query005 = (
+                            from T0 in db.Invoices
                             orderby T0.TransNo ascending
                             where T0.TransNo == "trans02"
                             select T0
-                           ).Take(1)
+                           ).AsNoTracking().Take(1)
             ;
 
             foreach (var invoice in query005)
@@ -109,14 +119,15 @@ namespace cobaef
                 ORDER BY T0.TransNo ASC
 
             */
-            var query006 = (from T0 in db.Invoices
+            var query006 = (
+                            from T0 in db.Invoices
                             orderby T0.TransNo ascending
                             where T0.TransNo == "trans02"
                             select new
                             {
                                 NomerInvoice = T0.TransNo
                             }
-                           ).Take(1)
+                           ).AsNoTracking().Take(1)
             ;
 
             foreach (var invoice in query006)
@@ -133,7 +144,8 @@ namespace cobaef
                 ORDER BY TransNo ASC
 
             */
-            var query007 = (from T0 in db.Invoices
+            var query007 = (
+                            from T0 in db.Invoices
                             join T1 in db.InvoiceItems on T0.Id equals T1.InvoiceId
                             orderby T0.TransNo ascending
                             where T0.TransNo == "trans02"
@@ -142,7 +154,7 @@ namespace cobaef
                                 NomerInvoice = T0.TransNo,
                                 KodeBarang = T1.ItemCode
                             }
-                           ).Take(1)
+                           ).AsNoTracking().Take(1)
             ;
 
             foreach (var invoice in query007)
@@ -159,7 +171,8 @@ namespace cobaef
                 ORDER BY TransNo ASC
 
             */
-            var query008 = (from T0 in db.Invoices
+            var query008 = (
+                            from T0 in db.Invoices
                             join T1 in db.InvoiceItems on T0.Id equals T1.InvoiceId into J01
                             from T1_0 in J01.DefaultIfEmpty()
                             orderby T0.TransNo ascending
@@ -169,7 +182,7 @@ namespace cobaef
                                 NomerInvoice = T0.TransNo,
                                 KodeBarang = T1_0.ItemCode
                             }
-                           ).Take(1)
+                           ).AsNoTracking().Take(1)
             ;
 
             foreach (var invoice in query008)
@@ -186,7 +199,8 @@ namespace cobaef
                 ORDER BY TransNo ASC
 
             */
-            var query009 = (from T0 in db.Invoices
+            var query009 = (
+                            from T0 in db.Invoices
                             from T1 in db.InvoiceItems
                             orderby T0.TransNo ascending
                             where T0.TransNo == "trans02"
@@ -195,7 +209,7 @@ namespace cobaef
                                 NomerInvoice = T0.TransNo,
                                 KodeBarang = T1.ItemCode
                             }
-                           ).Take(1000)
+                           ).AsNoTracking().Take(1000)
             ;
 
             foreach (var invoice in query009)
@@ -211,13 +225,15 @@ namespace cobaef
                 GROUP BY T0.ItemCode
 
             */
-            var query010 = from T0 in db.InvoiceItems
-                           group T0 by T0.ItemCode into GRP
-                           select new
-                           {
-                               KodeBarang = GRP.Key,
-                               TotalQty = GRP.Sum(x => x.Qty)
-                           }
+            var query010 = (
+                            from T0 in db.InvoiceItems
+                            group T0 by T0.ItemCode into GRP
+                            select new
+                            {
+                                KodeBarang = GRP.Key,
+                                TotalQty = GRP.Sum(x => x.Qty)
+                            }
+                           ).AsNoTracking()
             ;
 
             foreach (var invoice in query010)
@@ -233,15 +249,17 @@ namespace cobaef
                 GROUP BY T0.TransNo, T1.ItemCode 
 
             */
-            var query011 = from T0 in db.Invoices
-                           join T1 in db.InvoiceItems on T0.Id equals T1.InvoiceId
-                           group new { T0, T1 } by new { T0.TransNo, T1.ItemCode } into GRP
-                           select new
-                           {
-                               NomerInvoice = GRP.Key.TransNo,
-                               KodeBarang = GRP.Key.ItemCode,
-                               TotalQty = GRP.Sum(x => x.T1.Qty)
-                           }
+            var query011 = (
+                               from T0 in db.Invoices
+                               join T1 in db.InvoiceItems on T0.Id equals T1.InvoiceId
+                               group new { T0, T1 } by new { T0.TransNo, T1.ItemCode } into GRP
+                               select new
+                               {
+                                   NomerInvoice = GRP.Key.TransNo,
+                                   KodeBarang = GRP.Key.ItemCode,
+                                   TotalQty = GRP.Sum(x => x.T1.Qty)
+                               }
+                           ).AsNoTracking()
             ;
 
             foreach (var invoice in query011)
@@ -258,16 +276,18 @@ namespace cobaef
                 HAVING  SUM(T0.Qty)>2
 
             */
-            var query012 = from T0 in db.Invoices
-                           join T1 in db.InvoiceItems on T0.Id equals T1.InvoiceId
-                           group new { T0, T1 } by new { T0.TransNo, T1.ItemCode } into GRP
-                           where GRP.Sum(x => x.T1.Qty) > 2
-                           select new
-                           {
-                               NomerInvoice = GRP.Key.TransNo,
-                               KodeBarang = GRP.Key.ItemCode,
-                               TotalQty = GRP.Sum(x => x.T1.Qty)
-                           }
+            var query012 = (
+                            from T0 in db.Invoices
+                            join T1 in db.InvoiceItems on T0.Id equals T1.InvoiceId
+                            group new { T0, T1 } by new { T0.TransNo, T1.ItemCode } into GRP
+                            where GRP.Sum(x => x.T1.Qty) > 2
+                            select new
+                            {
+                                NomerInvoice = GRP.Key.TransNo,
+                                KodeBarang = GRP.Key.ItemCode,
+                                TotalQty = GRP.Sum(x => x.T1.Qty)
+                            }
+                           ).AsNoTracking()
             ;
 
             foreach (var invoice in query012)
@@ -298,11 +318,11 @@ namespace cobaef
                                NomerInvoice = GRP.Key.TransNo,
                                KodeBarang = GRP.Key.ItemCode,
                                TotalQty = GRP.Sum(x => x.T1.Qty)
-                           } 
-                           ).Take(2)
-                            //kalau di taruh di sini akan jadi sub query
-                            //.OrderBy(p => p.KodeBarang )   
-                            //.OrderByDescending(p => p.KodeBarang)
+                           }
+                           ).AsNoTracking().Take(2)
+            //kalau di taruh di sini akan jadi sub query
+            //.OrderBy(p => p.KodeBarang )   
+            //.OrderByDescending(p => p.KodeBarang)
             ;
 
             //foreach (var invoice in query013)
@@ -310,7 +330,7 @@ namespace cobaef
             //    Console.WriteLine($"013 -> {invoice.NomerInvoice}-{invoice.KodeBarang}-{invoice.TotalQty}");
             //}
 
-            //013 HAVING--WHERE--ORDERBY--CASE
+            //014 HAVING--WHERE--ORDERBY--CASE
             /*
                 SELECT  T0.TransNo AS NomerInvoice, T1.ItemCode AS KodeBarang, SUM(CASE WHEN T1.Qty<1 THEN 0 ELSE T1.Qty END) AS TotalQty 
                 FROM Invoices T0
@@ -335,12 +355,97 @@ namespace cobaef
                                TotalQty = GRP.Sum(x => x.T1.Qty < 2 ? 0 : x.T1.Qty)
                            }
 
-                           ).Take(2)
+                           ).AsNoTracking().Take(2)
             ;
 
             foreach (var invoice in query014)
             {
                 Console.WriteLine($"014 -> {invoice.NomerInvoice}-{invoice.KodeBarang}-{invoice.TotalQty}");
+            }
+
+            //015 UPDATE SATU TABLE -> Hanya di memory app 
+            /* 
+                UPDATE T0 
+                SET T0.Qty=CASE WHEN T0.Qty=1 THEN T0.Qty*10 ELSE T0.Qty END 
+                FROM InvoiceItems T0
+             */
+            //var query015 =  db.InvoiceItems.ToList().Select(c => { c.Qty = (((c.Qty.Equals(1)) ? c.Qty * 10 : c.Qty)); return c; }).ToList();
+            var query015 = (
+                               from T0 in db.InvoiceItems
+                               select T0
+                            ).AsNoTracking().ToList()
+                            .Select(c =>
+                            {
+                                c.Qty = ((c.Qty.Equals(1)) ? c.Qty * 10 : c.Qty);
+
+                                return c;
+                            })
+                            .ToList();
+            foreach (var invoiceItem in query015)
+            {
+                Console.WriteLine($"015 -> {invoiceItem.Id}-{invoiceItem.ItemCode}-{invoiceItem.Qty}");
+            }
+
+            //016 UPDATE JOIN TABLE -> Hanya di memory app 
+            /* 
+                UPDATE T1 
+                SET T1.Qty= CASE WHEN T1.Qty=3 THEN T1.Qty*10 ELSE T1.Qty END,
+	                T1.ItemCode='123'
+                FROM Invoices T0
+                INNER JOIN InvoiceItems T1 ON T0.Id=T1.Id
+                WHERE T0.TransNo='trans02'  
+            */
+
+            var query016 = (
+                          from T0 in db.Invoices
+                          join T1 in db.InvoiceItems on T0.Id equals T1.InvoiceId
+                          where T0.TransNo == "trans02"
+                          select T1
+                          ).AsNoTracking().ToList()
+                            .Select(c =>
+                            {
+                                c.Qty = ((c.Qty.Equals(3)) ? c.Qty * 10 : c.Qty);
+                                c.ItemCode = "123";
+
+                                return c;
+                            })
+                            .ToList();
+            ;
+            foreach (var invoiceItem in query016)
+            {
+                Console.WriteLine($"016 -> {invoiceItem.Id}-{invoiceItem.ItemCode}-{invoiceItem.Qty}");
+            }
+
+
+            //017 RUNING TOTAL -> Hanya di memory app 
+            /*
+            SELECT T0.Id, T0.ItemCode, T0.Qty, 
+                    (SELECT SUM(T0_.Qty) AS RunningTotal FROM InvoiceItems T0_ WHERE T0_.Id<=T0.Qty ) AS RunningTotal
+            FROM InvoiceItems T0
+            ORDER BY T0.Id  
+             * */
+            int? currentTotal = 0;
+            var query017 = (
+                               from T0 in db.InvoiceItems
+                               orderby T0.Id
+                               select T0
+
+                            ).AsNoTracking().ToList()
+                            .Select(c =>
+                            {
+                                currentTotal += c.Qty;
+                                return new
+                                {
+                                    Id = c.Id,
+                                    ItemCode = c.ItemCode,
+                                    Qty = c.Qty,
+                                    RunningTotal = currentTotal
+                                };
+                            })
+                            .ToList();
+            foreach (var invoiceItem in query017)
+            {
+                Console.WriteLine($"017 -> {invoiceItem.Id}-{invoiceItem.ItemCode}-{invoiceItem.Qty}-{invoiceItem.RunningTotal}");
             }
 
             Console.WriteLine("END YA");
